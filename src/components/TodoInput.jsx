092983089f79787c2d3e2fb3311f082c1fbb1311
Clifton <cliftonreed1986@gmail.com>
2024-10-10 21:34:26 -0700

    added the function of adding todo list item via key press of enter while in the input field.

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,12 +1,24 @@
+// TODO INPUT SECTION
+
+// allows the user to add a todo item to the todo list
+
 import React, { useState } from 'react'
 
 export default function TodoInput(props) {
   const {handleAddTodos, todoValue, setTodoValue} = props
   
+   // Function to handle adding todos on Enter key press
+   const handleKeyDown = (e) => {
+    if (e.key === 'Enter' && todoValue.trim() !== '') {
+      handleAddTodos(todoValue)
+      setTodoValue('')
+    }
+  }
+
   return (
     <header>
       <input value={todoValue} onChange={(e) => {setTodoValue(e.target.value)
-      }} placeholder="Enter todo..." />
+      }}  onKeyDown={handleKeyDown} placeholder="Enter todo..." />
       <button onClick={() => {
         handleAddTodos(todoValue)
         setTodoValue('')
